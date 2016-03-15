fa388bfd3b9709169d7f9c74e134c5c7eb3a5202
sabertazimi <sabertazimi@gmail.com>
2016-03-15 20:32:39 +0800

    update(C-Proj-MachineCode): machine code stored for diff types of var

--- C/Proj/machine_code.c
+++ C/Proj/machine_code.c
@@ -1,6 +1,6 @@
 /*************************************************************************
 	> File Name: machine_code.c
-	> Author: sabertazimi 
+	> Author: sabertazimi
 	> Mail: sabertazimi@gmail.com
     > License: Apache V2
 	> Created Time: 2016年03月15日 星期二 17时29分16秒
@@ -31,41 +31,75 @@
 #define FALSE 0
 #define LEN 8
 #define RESULTS_LEN 8
-#define DEMENSION 3 
+#define DEMENSION 3
 
 int main(void) {
-    int i, j, l, k = 0;
-    int x, y, z;
-    double dx, dy, dz;
-    int edges[] = {0, -1, INT_MIN, INT_MAX, 23333, INT_MAX / 23333, INT_MIN + 23333, INT_MAX - 23333};
-    int results[RESULTS_LEN];
+    // int i, j, l, k = 0;
+    // int x, y, z;
+    // double dx, dy, dz;
+    // int edges[] = {0, -1, INT_MIN, INT_MAX, 23333, INT_MAX / 23333, INT_MIN + 23333, INT_MAX - 23333};
+    // int results[RESULTS_LEN];
+    //
+    // for (i = 0;i < LEN;i++) {
+    //     x = edges[i];
+    //     dx = x;
+    //
+    //     for (j = 0;j < LEN;j++) {
+    //         y = edges[j];
+    //         dy = y;
+    //
+    //         for (l = 0;l < LEN;l++) {
+    //             z = edges[l];
+    //             dz = z;
+    //
+    //             // write expression to be test down
+    //             if (dx/dx == dy/dy) {
+    //                 ;
+    //             } else {
+    //                 results[k++] = x;
+    //                 results[k++] = y;
+    //                 results[k++] = z;
+    //             }
+    //         }
+    //     }
+    // }
+    //
+    // for (i = 0;i < k;i += DEMENSION) {
+    //     printf("x = %d, y = %d, z = %d\n", results[i], results[i + 1], results[i + 2]);
+    // }
 
-    for (i = 0;i < LEN;i++) { 
-        x = edges[i];
-        dx = x;
+    int i = -32768;
+    short s = 522;
+    unsigned u = 65530;
+    char c = '@';
+    float f = -1.1;
+    double df = 10.5;
+    unsigned char *p;
 
-        for (j = 0;j < LEN;j++) {
-            y = edges[j];
-            dy = y;
+    printf("%X\t%X\t%X\t%X\n", i, s, u, c);
 
-            for (l = 0;l < LEN;l++) {
-                z = edges[l];
-                dz = z;
-
-                // write expression to be test down
-                if (dx/dx == dy/dy) {
-                    ;
-                } else {
-                    results[k++] = x;
-                    results[k++] = y;
-                    results[k++] = z;
-                }
-            }
-        }
+    // small endian
+    p=(unsigned char *)&f;
+    for(i = sizeof(f) - 1;i >= 0;i--)
+    {
+        printf("%02X",p[i]);
     }
+    printf("\n");
 
-    for (i = 0;i < k;i += DEMENSION) {
-        printf("x = %d, y = %d, z = %d\n", results[i], results[i + 1], results[i + 2]);
+    // small endian
+    p=(unsigned char *)&df;
+    for(i = sizeof(df) - 1;i >= 0;i--)
+    {
+        printf("%02X",p[i]);
     }
+    printf("\n");
+
+    // int i = 0xFFFF0000;
+    // short s = 0xDFFC;
+    // unsigned u = 0xFFFFFFFA;
+    // char c = 0x2A;
+    // float f =  0xC4480000;
+    // double df = 0xC024800000000000;
+    //
+    // printf("%d\t%d\t%u\t%c\t%f\t%.60lf\n", i, s, u, c, f, df);
 }
-
