1dd20d8e9df73c1fb080c6935b41f72334c6fa11
sabertazimi <sabertazimi@gmail.com>
2016-03-19 23:10:56 +0800

    Update(Web-Proj-forgottenPasswords): complete layout v1.0 without js
    no js logic

--- Web/JavaScript/Proj/forgottenPasswords/js/index.js
+++ Web/JavaScript/Proj/forgottenPasswords/js/index.js
@@ -1,32 +1,28 @@
 $(document).ready(function () {
-    // 利用即时匿名函数,创建命名空间
-    (function (global) {
-        var $chatThread = $(".chat-thread"),
-            $inputAnchor = $(".chat-anchor")
-            $inputBtn = $("#chatInputBtn"),
-            $input = $("#chatInput"),
-            inputText = $input.val(),
-            inputHistory = [""],
-            getMessageHistory = [""],
-            inputMessageCallBack = function (e) {
-                inputText = $input.val();
+    var $startBtn = $(".btn-forgotten"),
+        $mobileBtn = $(".btn-mobile"),
+        $idcardBtn = $(".btn-idcard"),
+        $mobileForm = $("#mobileForm"),
+        $idcardForm = $("#idcardForm"),
+        $mainBody = $(".forgotten-main");
 
-                // XMLHttpRequest POST
+        $startBtn.click(function () {
+            $startBtn.addClass("hidden");
+            $mobileBtn.removeClass("hidden");
+            $idcardBtn.removeClass("hidden");
+        });
 
-                // XMLHttpRequest GET
+        $mobileBtn.click(function () {
+            $idcardBtn.addClass("hidden");
+            $mobileBtn.addClass("hidden");
+            $mainBody.removeClass("hidden");
+            $mobileForm.removeClass("hidden");
+        });
 
-                $chatThread.append('<li class="local">' + inputText + '</li>');
-
-                // 重新聚焦输入框, 并自动滚动至页面底部
-                $input.focus();
-
-                // FIX ME: 无效
-                $inputAnchor.click();
-            };
-
-        $inputBtn.click(inputMessageCallBack);
-
-        // $(input).input(keyboard === 'space' => inputBtnCallBack)
-
-    }(this));
+        $idcardBtn.click(function () {
+            $mobileBtn.addClass("hidden");
+            $idcardBtn.addClass("hidden");
+            $mainBody.removeClass("hidden");
+            $idcardForm.removeClass("hidden");
+        });
 });
