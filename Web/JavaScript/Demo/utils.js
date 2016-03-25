3d15c53cbc67be4ca63da4ffda935920f9d9de4b
sabertazimi <sabertazimi@gmail.com>
2016-03-25 21:11:46 +0800

    Add(Web-JS-Utils): get awesome small function - leftpad

--- Web/JavaScript/Demo/utils.js
+++ Web/JavaScript/Demo/utils.js
@@ -1,18 +1,31 @@
 var utils = {
-        // validate number format
-        toNum: function (num) {
-            return parseInt(Array.prototype.filter.call(num.toString(10), function (item) {
-                return item >= '0' && item <= '9';
-            }).join(''));
-        },
-        // delay function
-        sleep: function (numberMillis) {
-            var now = new Date(),
-                exitTime = now.getTime() + numberMillis;
-            while (true) {
-                now = new Date();
-                if (now.getTime() > exitTime)
-                return this;
-            }
+    // remove alpha between num
+    toNum: function (num) {
+        return parseInt(Array.prototype.filter.call(num.toString(10), function (item) {
+            return item >= '0' && item <= '9';
+        }).join(''));
+    },
+    // delay function
+    sleep: function (numberMillis) {
+        var now = new Date(),
+            exitTime = now.getTime() + numberMillis;
+        while (true) {
+            now = new Date();
+            if (now.getTime() > exitTime)
+            return this;
         }
-    };
+    },
+    leftpad: function (str, len, ch) {
+        var i = -1;
+
+        // handle users' input - don't trust users' input
+        str = String(str);
+        len = len - str.length;
+        if (!ch && ch !== 0) ch = ' ';
+
+        while (++i < len) {
+            str = ch + str;
+        }
+        return str;
+    }
+};
