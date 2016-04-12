1547e12ee1899801595c82b803b42cca20312da4
sabertazimi <sabertazimi@gmail.com>
2016-04-12 23:16:40 +0800

    Update(Nodejs-Proj-Blog): add node.gitignore

--- Nodejs/Proj/blog/app.js
+++ Nodejs/Proj/blog/app.js
@@ -1,12 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes/index'),
+    users = require('./routes/users'),
+    accounts = require('./routes/accounts');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/accounts', accounts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Nodejs/Proj/blog/routes/accounts.js
+++ Nodejs/Proj/blog/routes/accounts.js
@@ -1,5 +1,9 @@
 var express = require('express'),
-    router = express.router();
+    router = express.Router();
+
+router.get('/', function (req, res) {
+    return res.send('This is accounts interface.');
+});
 
 router.get('/signin', function (req, res) {
     return res.send('This is sign in interface.');
