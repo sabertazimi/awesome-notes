8dd8234eba8cd47d39155aaf51272e436e462df3
sabertazimi <sabertazimi@gmail.com>
2016-04-12 20:35:25 +0800

    Update(Nodejs-Proj): change title, add title-icon, add /hello route

--- Nodejs/Proj/blog/routes/index.js
+++ Nodejs/Proj/blog/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sabertazimi's Blog' });
+  res.render('index', { title: 'Sabertazimi\'s Blog' });
 });
 
 module.exports = router;
