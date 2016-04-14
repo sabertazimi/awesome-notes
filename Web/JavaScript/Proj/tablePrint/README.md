160a44ac108d48e59d330684c1d219221d3d96e0
sabertazimi <sabertazimi@gmail.com>
2016-04-14 21:06:07 +0800

    Add(Web-JS-Proj): start working on tablePrint

---
+++ Web/JavaScript/Proj/tablePrint/README.md
@@ -0,0 +1,14 @@
+## IFE Task 26
+
+### Run
+
+```shell
+$ bower install
+$ npm install
+$ gulp
+$ firefox ./dist/index.html
+```
+
+### License
+
+MIT [@sabertazimi](https://github.com/sabertazimi/)
--- Web/Template/simpleLayout/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    var $startBtn = $(".btn-custom");
-    
-        $startBtn.click(function () {
-            $startBtn.addClass("hidden");
-        }).pause();
-});
