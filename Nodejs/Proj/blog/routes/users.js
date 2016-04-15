16f4ae50bf6f9ebd11507b1d13f84f2dbadec105
sabertazimi <sabertazimi@gmail.com>
2016-04-15 17:34:19 +0800

    Remove(Nodejs-Proj-Blog): move blog to st-proj repo

--- Nodejs/Proj/blog/routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('This is users interface.');
-});
-
-module.exports = router;
