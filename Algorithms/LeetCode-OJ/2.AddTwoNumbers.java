028dc960da9f68047b564c19432ba7ea7752baec
sabertazimi <sabertazimi@gmail.com>
2016-05-01 21:19:43 +0800

    Add(LeetCode): Start working on Add Two Numbers
    It's a chanllenge about link list.

---
+++ Algorithms/LeetCode-OJ/2.AddTwoNumbers.java
@@ -0,0 +1,12 @@
+/*
+* @Author: sabertazimi
+* @Date:   2016-05-01 21:19:04
+* @Last Modified by:   sabertazimi
+* @Last Modified time: 2016-05-01 21:19:15
+*/
+
+public class java {
+    public static void main(String[] args) {
+
+    }
+}
