467f01c9ab9dfa144c21347632d2663cd17893be
sabertazimi <sabertazimi@gmail.com>
2016-10-05 22:06:33 +0800

    update(lang-cpp): learn more about stack/queue/priority_queue

---
+++ computerScience/algorithms/demos/basic/cpp_stl_stack_queue.cc
@@ -0,0 +1,12 @@
+/*
+ * cpp_stl_stack_queue.cc
+ * Copyright (C) 2016 sabertazimi <sabertazimi@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+/*
+ * stk.push/pop/top();
+ * que.push/pop/front();
+ * prio_que.push/pop/top();
+ */
