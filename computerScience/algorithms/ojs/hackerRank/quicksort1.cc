8c4a25ef9c4b406b6aa7d2df37271e97f44ba55b
sabertazimi <sabertazimi@gmail.com>
2016-11-13 18:12:43 +0800

    add(oj-quicksort1): try to implement partition function

--- computerScience/algorithms/ojs/hackerRank/quicksort1.cc
+++ computerScience/algorithms/ojs/hackerRank/quicksort1.cc
@@ -15,6 +15,30 @@ using namespace std;
 
 void partition(vector<int> arr) {
     // enter code for partitioning and printing here.
+    int guard = arr[0],
+        left = 1,
+        right = arr.size() - 1;
+
+    while (left <= right) {
+        while (arr[left] < guard) left++;
+        while (arr[right] > guard) right--;
+
+        int tmp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = tmp;
+
+        left++;
+        right--;
+    }
+
+    int tmp = arr[right];
+    arr[right] = arr[0];
+    arr[0] = tmp;
+
+    for (int i = 0; i < arr.size() - 1; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << arr[arr.size() - 1];
 }
 
 int main(void) {
