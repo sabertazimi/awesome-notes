23ef03f0e0407f932df44f681158b0e8ff64acd9
sabertazimi <sabertazimi@gmail.com>
2016-11-14 16:28:18 +0800

    update(oj-select2): complete select2 algorithm implementation
    issue sabertazimi/awesome-notes#88

--- computerScience/algorithms/ojs/hackerRank/select2.cc
+++ computerScience/algorithms/ojs/hackerRank/select2.cc
@@ -61,9 +61,10 @@ static int partition(vector<int> &arr, int lo, int hi) {
 /// \param hi  highest limit of array
 /// \return index of kth element
 static int _select2(vector<int> &arr, int kth, int lo, int hi) {
-    int n  = arr.size();
 
     while (1) {
+        int n  = hi - lo + 1;       ///< number of elements locating in lo:hi range
+
         if (n <= r) {
             insertionSort(arr, lo, hi);
             return (kth + lo - 1);
@@ -71,16 +72,25 @@ static int _select2(vector<int> &arr, int kth, int lo, int hi) {
 
         /* start of getting median of medians */
 
+        int glo, ghi;               ///< lo and hi of each group-array
+        int median;                 ///< index of median of each group-array
+        int middle = ceil(r/2.0);   ///< constant represent middle index of group-array
+
         for (int i = 0; i < n/r; i++) {
             // sort each group-array(sub-array)
-            insertionSort(arr, lo + i*r, lo + (i + 1)*r - 1);
+            glo = lo + i*r;
+            ghi = lo + (i + 1)*r - 1;
+            insertionSort(arr, glo, ghi);
 
             // collect medians in front part of arr[lo:hi];
-            swap(arr[lo + i], arr[lo + i*r + ceil((double)r/2.0) - 1]);
+            median = lo + i*r + middle - 1;
+            swap(arr[lo + i], arr[median]);
         }
 
         // select ceil(n/r/2) largest element from medians-array(in front part of arr[lo:hi])
-        int spot = _select2(arr, ceil((double)n/r/2.0), lo, lo + n/r - 1);
+        int mmiddle = ceil(n/r/2.0);    ///< middle index of medians-array
+        int mhi     = lo + n/r - 1;     ///< upper limit of medians-array
+        int spot = _select2(arr, mmiddle, lo, mhi);
 
         // move median of medians-array to arr[lo]
         swap(arr[lo], arr[spot]);
@@ -119,34 +129,34 @@ int main(void) {
     cin >> n;
 
     vector<int> arr(n);
+    vector<int> brr(n);
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
+        brr[i] = arr[i];
     }
+
+    insertionSort(brr, 0, brr.size() - 1);
+
+#ifdef DEBUG
+    for (int i = 0; i < (int)brr.size() - 1; i++) {
+        cout << brr[i] << " ";
+    }
+    cout << brr[brr.size() - 1] << endl;
+#endif // DEBUG
 
     do {
         cin >> kth;
-    } while (kth < 1);
+        cout << "[unsorted] the " << kth << " largest element = " << select2(arr, kth) << endl;
 
-    cout << "[unsorted] arr[" << kth << "] = " << select2(arr, kth) << endl;
+    #ifdef DEBUG
+        for (int i = 0; i < (int)arr.size() - 1; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << arr[arr.size() - 1] << endl;
+    #endif // DEBUG
 
-#ifdef DEBUG
-    for (int i = 0; i < (int)arr.size() - 1; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << arr[arr.size() - 1] << endl;
-#endif // DEBUG
-
-    // insertionSort(arr, 0, arr.size() - 1);
-
-    cout << "[ sorted ] arr[" << kth << "] = " << select2(arr, kth) << endl;
-
-#ifdef DEBUG
-    for (int i = 0; i < (int)arr.size() - 1; i++) {
-        cout << arr[i] << " ";
-    }
-    cout << arr[arr.size() - 1] << endl;
-#endif // DEBUG
+    } while (kth > 0);
 
     return 0;
 }
