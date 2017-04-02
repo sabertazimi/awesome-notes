29768928cfa6e80150f3cca7e23eac63aa7dcc54
sabertazimi <sabertazimi@gmail.com>
2017-04-02 19:27:14 +0800

    add(dotfiles-ss): add shadowsocks set up script

---
+++ programming/linux/setUp/gfw/ss_setup.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#
+# ss_setup.sh
+# Copyright (C) 2017 sabertazimi <sabertazimi@avalon>
+#
+# Distributed under terms of the MIT license.
+#
+
+yum install python-setuptools && easy_install pip
+pip install shadowsocks
+echo "nohup sslocal -c /etc/shadowsocks.json /dev/null 2>&1 &" /etc/rc.local
+nohup sslocal -c /etc/shadowsocks.json /dev/null 2>&1 &
+
