df1c4e874adb8a60089ccfd6601f170e66856b11
sabertazimi <sabertazimi@gmail.com>
2021-09-01 01:43:33 +0800

    docs(website-theme): change dark code theme to `dracula`

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
