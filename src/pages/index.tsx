d7a6e91e02764bb37e67df0999efdcbe8315feca
Sabertaz <sabertazimi@gmail.com>
2022-05-30 16:58:55 +0800

    feat(katex): add katex support for markdown file (#318)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
+import 'katex/dist/katex.css';
 import React from 'react';
 import HomepageFeatures from '../components/HomepageFeatures';
 import styles from './index.module.css';
