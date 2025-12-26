ead99da237524227726af2c67c0f91910d9d194e
sabertazimi <sabertazimi@gmail.com>
2025-12-26 18:15:03 +0800

    chore(lint): remove MDX from lint-staged config

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -3,7 +3,7 @@
  * @type {import('lint-staged').Configuration}
  */
 export default {
-  '*.{md,mdx,ts,tsx,css}': 'eslint --fix',
-  '*.{md,mdx,css}': 'stylelint --fix',
+  '*.{md,ts,tsx,css}': 'eslint --fix',
+  '*.{md,css}': 'stylelint --fix',
   '*.{md,mdx}': 'markdownlint --fix',
 }
